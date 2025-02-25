432274d605d90073cc65f1bddfec7e2d5463d88e
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-26 02:10:51 +0900

    Update get_web_service_app()

--- ratsnlp/nlpbook/generation/deploy.py
+++ ratsnlp/nlpbook/generation/deploy.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 
 
-def get_web_service_app(inference_fn, is_colab=True):
-    app = Flask(__name__, template_folder='')
+def get_web_service_app(inference_fn, template_folder, server_page):
+    app = Flask(__name__, template_folder=template_folder)
     from flask_cors import CORS
     CORS(app)
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return render_template(server_page)
 
     @app.route('/api', methods=['POST'])
     def api():
